c28c9562c91bfede3eb3b4fad56c850af1e96b03
Gauri Joshi <gauri728@gmail.com>
2022-04-21 15:54:58 -0700

    Checks integers for under 60

--- friends.rb
+++ friends.rb
@@ -1,6 +1,11 @@
 lucky_numbers = [7, 33, 50, 77, 12, 63, 80, 99]
+# lucky_numbers.each do |num|
+#   if num > 30
+#   puts "#{num}"
+# end
+#end
 lucky_numbers.each do |num|
-  if num > 30
-  puts "#{num}"
-end
+  if num < 60
+    puts "#{num}"
+  end
 end
